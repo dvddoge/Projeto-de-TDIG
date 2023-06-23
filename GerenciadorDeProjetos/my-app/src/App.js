b4521a2b09aabfeff17c3e786e7a682da6309f96
dvdJr <95938301+dvddoge@users.noreply.github.com>
2023-06-23 18:54:08 +0000

    Ajuste no app.js

--- GerenciadorDeProjetos/my-app/src/App.js
+++ GerenciadorDeProjetos/my-app/src/App.js
@@ -1,11 +1,26 @@
-import LoginForm from './components/Login';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import AuthenticatedAxios from './components/AuthenticatedAxios';
+
+const Logout = () => {
+  localStorage.removeItem('jwtToken');
+  return <Redirect to="/login" />;
+}
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={LoginForm} />
+        <Route path="/logout" component={Logout} />
+        <PrivateRoute path="/profile" component={UserProfile} /> {/* substitua pelo componente de perfil de usuário */}
+        <PrivateRoute path="/home" component={UserHome} /> {/* substitua pelo componente da página inicial do usuário */}
+        <PrivateRoute path="/project-management" component={ProjectManagement} /> {/* substitua pelo componente de gerenciamento de projetos */}
+        <Route path="*" component={() => "404 Not Found"} />
+      </Switch>
+    </Router>
   );
 }
 
