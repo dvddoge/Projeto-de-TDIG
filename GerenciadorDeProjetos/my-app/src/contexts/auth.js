9d65a6103c3d4be7a289a5e50af80697c9204e40
dvdJr <95938301+dvddoge@users.noreply.github.com>
2023-06-23 23:04:40 +0000

    signin adicionado

--- GerenciadorDeProjetos/my-app/src/contexts/auth.js
+++ GerenciadorDeProjetos/my-app/src/contexts/auth.js
@@ -18,5 +18,23 @@ export const AuthProvider = ({ children}) => {
         }
     }, []);
 
+    const signin = (email, password) => {
+        const userStorage = JSON.parse(localStorage.getItem("users_db"));
+        const hasUser = userStorage?.filter((user) => user.email === email);
+
+        if(hasUser?.lenght) {
+            if(hasUser[0].email === email && hasUser[0].password === password) {
+                const token = Math.random().toString(36).substring(2);
+                localStorage.setItem("user_token", JSON.stringify({email, token}));
+                setUser({email, password});
+                return;
+            } else {
+                return "E-mail ou senha incorretos";
+            }
+
+        } else {
+            return "Usuário não cadastrado";
+        }
+    };
     return <AuthContext.Provider>{children}</AuthContext.Provider>
-}
+};
